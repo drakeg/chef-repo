f97081ea74363f7903cc343d148bfbc8a777c504
Mad Mallard Solutions <greg@madmallards.com>
2013-02-18 23:32:10 -0500

    Update the monit cookbook to install the monit package (Ubuntu), check its status, and start it.

--- cookbooks/monit/recipes/default.rb
+++ cookbooks/monit/recipes/default.rb
@@ -2,7 +2,13 @@
 # Cookbook Name:: monit
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, Mad Mallard Solutions 
 #
 # All rights reserved - Do Not Redistribute
 #
+package 'monit'
+
+service 'monit' do
+  supports [:status]
+  actions :start
+end
